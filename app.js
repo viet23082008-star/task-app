8bbb3eaa77e015d3c8172ddccccc64026c0fe25a
viet <viet23082008@gmail.com>
2026-09-03 09:29:03 +0700

    C5: Hello World

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 function hienThiTask() { console.log('Task 1'); }
-function xoaTatCa() { // CODE LOI NGUY HIEM! }
+function xoaTatCa() {  } // CODE LOI NGUY HIEM!
+function xinChao() {
+    console.log("Hello World");
+}
